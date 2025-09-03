64118feccba2c0e4c54f6ae13d65ffa1661742a7
timmy quan <timmyquan123@aol.com>
2025-09-03 07:44:46 -0700

    update image domains for CDN

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'd18xm8l1snf084.cloudfront.net' }, // or your cdn domain
+      { protocol: 'https', hostname: 'cdn.inschekr.com' },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
